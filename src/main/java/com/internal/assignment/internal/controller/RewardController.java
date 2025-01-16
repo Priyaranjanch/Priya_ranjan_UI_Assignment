83689f6d3fe1443bb61d9a7e2eba242cf02605b2
Priyaranjanch <101156247+Priyaranjanch@users.noreply.github.com>
2025-01-16 17:08:16 +0530

    Reward update (#4)
    * Optimized code
    
    * Modified and optimized code for reward layer

--- src/main/java/com/internal/assignment/internal/controller/RewardController.java
+++ src/main/java/com/internal/assignment/internal/controller/RewardController.java
@@ -1,12 +1,15 @@
 package com.internal.assignment.internal.controller;
 
+import com.internal.assignment.internal.Entity.RewardPoints;
 import com.internal.assignment.internal.service.RewardService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
 import java.util.Map;
 
 @RestController
@@ -21,5 +24,21 @@ public class RewardController {
         return rewardService.calculateRewards(customerId);
     }
 
-}
+    @GetMapping("/all/{customerId}")
+    public ResponseEntity<List<RewardPoints>> getAllRewardPoints(@PathVariable Long customerId) {
+        List<RewardPoints> rewardPoints = rewardService.getAllRewardPoints(customerId);
+        return ResponseEntity.ok(rewardPoints);
+    }
 
+    /**
+     * Calculate and retrieve the total reward points for a customer.
+     *
+     * @param customerId The ID of the customer.
+     * @return Total reward points for the customer.
+     */
+    @GetMapping("/total/{customerId}")
+    public ResponseEntity<Integer> calculateTotalRewards(@PathVariable Long customerId) {
+        int totalRewards = rewardService.calculateTotalRewardPoints(customerId);
+        return ResponseEntity.ok(totalRewards);
+    }
+}
